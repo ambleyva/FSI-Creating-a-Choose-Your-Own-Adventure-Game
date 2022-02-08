4a755661cc841a71665f4b9fd48dbbd1abdf4182
ambleyva <ambleyva24@gmail.com>
2022-02-07 19:20:58 -0500

    changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ It scampers off down a small hole, just large enough for you to crawl through.
 Do you follow it, or continue on your path?`)
     if (secondAnswer === 'follow it') {
         let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
-        if(secondAnswer === 'stay') {
+        if(firstAnswer === 'stay') {
             let secondAnswer = window.prompt('You live happily amongs the cats for the rest of your days.')
         } else if (secondAnswer === 'spread the word') {
             let secondAnswer = window.alert('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
