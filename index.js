565e6966f9ecbd3c84fdcfeecb29fd7e75e82428
ambleyva <ambleyva24@gmail.com>
2022-02-07 20:02:48 -0500

    test

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
- let window.prompt('Do you head left or right?')
+let firstAnswer = window.prompt ('do you head left or right')
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
     if (secondAnswer === 'follow it') {
         let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
-        if(firstAnswer === 'stay') {
-            let secondAnswer = window.prompt('You live happily amongs the cats for the rest of your days.')
-        } else if (secondAnswer === 'spread the word') {
-            let secondAnswer = window.alert('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
-        }
+        if(thirdanswer === 'stay') {
+            let thirdanswer = window.prompt('You live happily amongs the cats for the rest of your days.')
+        } else if (thirdanswer === 'spread the word') {
+            let thirdanswer = window.alert('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
+    else if (secondAnswer === 'continue') {
+
+    }}
 
     }
 } else if(firstAnswer === 'right') {
