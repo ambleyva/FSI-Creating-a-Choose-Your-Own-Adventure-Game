f332323b8225503ba141128ca0cf31b16a0ba009
ambleyva <ambleyva24@gmail.com>
2022-02-07 19:38:38 -0500

    test

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 window.prompt('Do you head left or right?')
-let firstAnswer  = window.prompt('Do you head left or right?')
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
@@ -17,5 +16,7 @@ Do you follow it, or continue on your path?`)
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
-
+    if(firstAnswer === 'chest'){  
+    }
+    else if (secondAnswer === 'away'){}
 }
